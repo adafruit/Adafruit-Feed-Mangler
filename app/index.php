452173d6041f6e1acc32b651f016962c2c2db7e8
Brennen Bearnes <bbearnes@gmail.com>
2015-05-06 17:11:02 +0000

    stupid json viewer

--- app/index.php
+++ app/index.php
@@ -5,14 +5,20 @@ use \SparkLib\Fail;
 
 $app = new \Slim\Slim;
 
-$app->response->headers->set('Content-Type', 'application/atom+xml');
 
-$app->get('/search/:term', function ($term) {
+$app->get('/search/:term', function ($term) use ($app) {
+  $app->response->headers->set('Content-Type', 'application/atom+xml');
   print \Adafruit\FeedMangler\Atom::searchResults($term);
 });
 
-$app->get('/', function () {
+$app->get('/', function () use ($app) {
+  $app->response->headers->set('Content-Type', 'application/atom+xml');
   print \Adafruit\FeedMangler\Atom::searchResults('adafruit');
+});
+
+$app->get('/json', function () use ($app) {
+  $app->response->headers->set('Content-Type', 'text/html');
+  print '<pre>' . print_r(\Adafruit\FeedMangler\Mangler::getSearchData('adafruit'), 1) . '</pre>';
 });
 
 $app->run();
